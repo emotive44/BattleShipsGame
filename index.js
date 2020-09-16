abc761f8ab32778ebc753da4241ec7fb5d668716
marko streleshki <marko_streleshki_96@abv.bg>
2020-09-16 16:20:24 +0300

    create a initial grid

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log(1);
+// create a initial grid
+(() => {
+  const battleGrid = document.getElementById('battle-board');
+
+  for (let i = 1; i <= 100; i++) {
+    const span = document.createElement('span');
+    span.innerHTML = '.';
+    span.id = `dot-${i}`;
+
+    battleGrid.appendChild(span);
+  }
+})();
